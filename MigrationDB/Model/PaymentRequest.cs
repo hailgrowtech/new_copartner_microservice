2e15eeacac4801677a8a9b7dbc54f5b58e7658bd
Akash Khairal <akashkhairal@gmail.com>
2024-04-22 15:33:45 +0530

    razopay gateway

--- MigrationDB/Model/PaymentRequest.cs
+++ MigrationDB/Model/PaymentRequest.cs
@@ -2,12 +2,9 @@
 {
     public class PaymentRequest
     {
-        public string MerchantId { get; set; }
-        public string OrderId { get; set; }
+        
         public decimal Amount { get; set; }
-        public string Currency { get; set; }
-        public string CallbackUrl { get; set; }
-        public string CustomerId { get; set; }
+
     }
 
 }
