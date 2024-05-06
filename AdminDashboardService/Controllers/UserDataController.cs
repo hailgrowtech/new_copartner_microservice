eeff4a33a2a0b8d00884b3c1d9be39909b76be2f
Akash Khairal <akashkhairal@gmail.com>
2024-05-06 17:07:32 +0530

    user listing api

---
+++ AdminDashboardService/Controllers/UserDataController.cs
@@ -0,0 +1,46 @@
+﻿using AdminDashboardService.Logic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminDashboardService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserDataController : ControllerBase
+    {
+        private readonly IUserDataListingBusinessProcessor _logic;
+        private readonly ILogger<UserDataController> _logger;
+
+        public UserDataController(IUserDataListingBusinessProcessor logic, ILogger<UserDataController> logger)
+        {
+            _logic = logic;
+            _logger = logger;
+        }
+
+        [HttpGet("UserDataListing", Name = "GetUserDataListing")]
+        public async Task<object> GetUserDataListing()
+        {
+            _logger.LogInformation("Fetching Dashboard GetUserDataListing Details Data..");
+            var userListingData = await _logic.GetUserListing();
+            return Ok(userListingData);
+        }
+
+        [HttpGet("UserFirstTimePaymentListing", Name = "GetUserFirstTimePaymentListing")]
+        public async Task<object> GetUserFirstTimePaymentListing()
+        {
+            _logger.LogInformation("Fetching Dashboard GetUserFirstTimePaymentListing Details Data..");
+            var userListingData = await _logic.GetFirstTimePaymentListing();
+            return Ok(userListingData);
+        }
+
+        [HttpGet("UserSecondTimePaymentListing", Name = "GetUserSecondTimePaymentListing")]
+        public async Task<object> GetUserSecondTimePaymentListing()
+        {
+            _logger.LogInformation("Fetching Dashboard GetUserSecondTimePaymentListing Details Data..");
+            var userListingData = await _logic.GetSecondTimePaymentListing();
+            return Ok(userListingData);
+        }
+
+
+    }
+}
---
+++ AdminDashboardService/Handlers/GetUserFirstTimePaymentListingHandler.cs
@@ -0,0 +1,40 @@
+﻿using AdminDashboardService.Dtos;
+using AdminDashboardService.Queries;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MigrationDB.Data;
+
+namespace AdminDashboardService.Handlers
+{
+    public class GetUserFirstTimePaymentListingHandler : IRequestHandler<GetUserFirstTimePaymentListingQuery, IEnumerable<UserFirstTimePaymentListingDto>>
+    {
+        private readonly CoPartnerDbContext _dbContext;
+        public GetUserFirstTimePaymentListingHandler(CoPartnerDbContext dbContext) => _dbContext = dbContext;
+
+        public async Task<IEnumerable<UserFirstTimePaymentListingDto>> Handle(GetUserFirstTimePaymentListingQuery request, CancellationToken cancellationToken)
+        {
+            var usersWithFirstPayment = await _dbContext.Users
+                    .Where(u => !u.IsDeleted)
+                    .Join(_dbContext.Subscribers,
+                        u => u.Id,
+                        sub => sub.UserId,
+                        (u, sub) => new { User = u, Subscriber = sub })
+                    .GroupBy(join => join.User, join => join.Subscriber, (user, subscribers) => new
+                    {
+                        User = user,
+                        Subscribers = subscribers.ToList() // Materialize subscribers into a list
+                    })
+                    .Where(group => group.Subscribers.Count == 1) // Filter for users with exactly one subscriber (one payment)
+                    .Select(group => new UserFirstTimePaymentListingDto
+                    {
+                        Date = group.Subscribers.FirstOrDefault().CreatedOn, // Assuming CreatedOn represents subscriber creation date
+                        Mobile = group.User.MobileNumber,
+                        Name = group.User.Name,
+                        Payment = group.Subscribers.FirstOrDefault().TotalAmount // Assuming TotalAmount represents the payment amount
+                    })
+                    .ToListAsync();
+
+            return  usersWithFirstPayment;
+        }
+    }
+}
