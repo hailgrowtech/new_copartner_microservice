37a19c81c7b49e1c9fcff4fceeea443a5fad3900
DEEPAK NEGI <deepak.negi@alecom.se>
2024-05-22 19:25:09 +0530

    AWS Storage

---
+++ AdminDashboardService/Controllers/AWSStorageController.cs
@@ -0,0 +1,42 @@
+﻿using AdminDashboardService.Logic;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AdminDashboardService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AWSStorageController : ControllerBase
+{
+    private readonly IAWSStorageBusinessProcessor _logic;
+    private readonly ILogger<AWSStorageController> _logger;
+    private readonly IConfiguration _configuration;
+
+    public AWSStorageController(IAWSStorageBusinessProcessor logic, ILogger<AWSStorageController> logger,IConfiguration configuration)
+    {
+        _logic = logic;
+        _logger = logger;
+        _configuration = configuration;
+    }
+    /// <summary>
+    /// Upload Files in Amazon S3 Staorage 
+    /// </summary>
+    /// <param name="file"></param>
+    /// <param name="prefix">Name of Folder For Images user Images for video use Videos</param>
+    /// <returns></returns>
+    [HttpPost]
+    public async Task<IActionResult> Post(IFormFile file, string? prefix)
+    {
+        _logger.LogInformation("Uploading Data in AWS S3 Bucket..");
+        string bucketName = _configuration["AWSCredentials:BucketName"];
+        var response = await _logic.Post(file, prefix, bucketName);
+        if (response.IsSuccess)
+        {
+            return Ok(response);
+        }
+        return NotFound(response);
+    }
+}
---
+++ AdminDashboardService/Logic/AWSStorageBusinessProcessor.cs
@@ -0,0 +1,181 @@
+﻿using AutoMapper;
+using CommonLibrary;
+using CommonLibrary.CommonDTOs;
+using MediatR;
+using Microsoft.AspNetCore.JsonPatch;
+using AdminDashboardService.Dtos;
+using MigrationDB.Model;
+using AdminDashboardService.Queries;
+using AdminDashboardService.Commands;
+using Amazon.S3;
+using Amazon.S3.Model;
+using static MassTransit.ValidationResultExtensions;
+
+namespace AdminDashboardService.Logic;
+public class AWSStorageBusinessProcessor : IAWSStorageBusinessProcessor
+{
+    private readonly ISender _sender;
+    private readonly IMapper _mapper;
+    private readonly IAmazonS3 _s3Client;
+
+    public AWSStorageBusinessProcessor(ISender sender, IMapper mapper, IAmazonS3 s3Client)
+    {
+        this._sender = sender;
+        this._mapper = mapper;
+        this._s3Client = s3Client;
+    }
+
+    public async Task<ResponseDto> Get(int page = 1, int pageSize = 10)
+    {        
+            var expertsList = await _sender.Send(new GetBlogQuery(page , pageSize));
+            var expertsReadDtoList = _mapper.Map<List<BlogReadDto>>(expertsList);
+            return new ResponseDto()
+            {
+                IsSuccess = true,
+                Data = expertsReadDtoList,
+            };           
+    }
+    public async Task<ResponseDto> Get(Guid id)
+    {
+        var experts = await _sender.Send(new GetBlogByIdQuery(id));
+        if (experts == null)
+        {
+            return new ResponseDto()
+            {
+                IsSuccess = false,
+                Data = null,
+                ErrorMessages = new List<string>() { AppConstants.Expert_ExpertNotFound }
+            };
+        }
+        var expertsReadDto = _mapper.Map<BlogReadDto>(experts);
+        return new ResponseDto()
+        {
+            IsSuccess = true,
+            Data = expertsReadDto,
+        };
+    }
+
+    public async Task<ResponseDto> Delete(Guid id)
+    {
+        var expert = await _sender.Send(new DeleteBlogCommand(id));
+        var expertReadDto = _mapper.Map<ResponseDto>(expert);
+        return expertReadDto;
+    }
+
+    public async Task<ResponseDto> Post(IFormFile file, string? prefix, string bucketName)
+    {
+        var bucketExists = await _s3Client.DoesS3BucketExistAsync(bucketName);
+        if (!bucketExists)
+        {
+            return new ResponseDto()
+            {
+                IsSuccess = false,
+                ErrorMessages = new List<string>() { $"Bucket {bucketName} does not exist." }
+            };
+        }
+
+        // Upload the file to Amazon S3
+        var request = new PutObjectRequest()
+        {
+            BucketName = bucketName,
+            Key = string.IsNullOrEmpty(prefix) ? file.FileName : $"{prefix?.TrimEnd('/')}/{file.FileName}",
+            InputStream = file.OpenReadStream()
+        };
+        request.Metadata.Add("Content-Type", file.ContentType);
+        await _s3Client.PutObjectAsync(request);
+
+        // Generate a presigned URL for the uploaded file
+        var presignedUrlRequest = new GetPreSignedUrlRequest()
+        {
+            BucketName = bucketName,
+            Key = request.Key,
+            Expires = DateTime.UtcNow.AddMinutes(1) // Set the expiration time for the presigned URL
+        };
+        string presignedUrl = _s3Client.GetPreSignedURL(presignedUrlRequest);
+
+        // Create an instance of AWSStorageReadDto and assign the presigned URL to it
+        var resultDto = new AWSStorageReadDto
+        {
+            PresignedUrl = presignedUrl
+        };
+
+        // Prepare the response DTO
+        var responseDto = new ResponseDto()
+        {
+            Data = resultDto,
+            DisplayMessage = $"File {prefix}/{file.FileName} uploaded to S3 successfully!"
+        };
+
+        return responseDto;
+    }
+
+
+
+    public async Task<ResponseDto> Put(Guid id, BLogCreateDto request)
+    {
+        var blogs = _mapper.Map<Blog>(request);
+
+        var existingblogs = await _sender.Send(new GetBlogByIdQuery(id));
+        if (existingblogs == null)
+        {
+            return new ResponseDto()
+            {
+                IsSuccess = false,
+                Data = _mapper.Map<BlogReadDto>(existingblogs),
+                ErrorMessages = new List<string>() { AppConstants.Common_NoRecordFound }
+            };
+        }
+        blogs.Id = id; // Assigning the provided Id to the experts
+        var result = await _sender.Send(new PutBlogCommand(blogs));
+        if (result == null)
+        {
+            return new ResponseDto()
+            {
+                IsSuccess = false,
+                Data = _mapper.Map<BlogReadDto>(existingblogs),
+                ErrorMessages = new List<string>() { AppConstants.AffiliatePartner_FailedToCreateAffiliatePartner }
+            };
+        }
+
+        var resultDto = _mapper.Map<BlogReadDto>(result);
+        return new ResponseDto()
+        {
+            Data = resultDto,
+            DisplayMessage = AppConstants.Expert_ExpertCreated
+        };
+    }
+
+    public async Task<ResponseDto> Patch(Guid Id, JsonPatchDocument<BLogCreateDto> request)
+    {
+        var blogs = _mapper.Map<Blog>(request);
+
+        var existingBlogs = await _sender.Send(new GetBlogByIdQuery(Id));
+        if (existingBlogs == null)
+        {
+            return new ResponseDto()
+            {
+                IsSuccess = false,
+                //   Data = _mapper.Map<ExpertsReadDto>(existingExperts),
+                ErrorMessages = new List<string>() { AppConstants.Expert_ExpertNotFound }
+            };
+        }
+
+        var result = await _sender.Send(new PatchBlogCommand(Id, request, existingBlogs));
+        if (result == null)
+        {
+            return new ResponseDto()
+            {
+                IsSuccess = false,
+                Data = _mapper.Map<BlogReadDto>(existingBlogs),
+                ErrorMessages = new List<string>() { AppConstants.Expert_FailedToUpdateExpert }
+            };
+        }
+
+        return new ResponseDto()
+        {
+            Data = _mapper.Map<BlogReadDto>(result),
+            DisplayMessage = AppConstants.Expert_ExpertUpdated
+        };
+    }
+}
+
