19f818afdc95c9c2dddd1416e1eb30bef2f8da8e
DEEPAK NEGI <deepak.negi@alecom.se>
2024-05-26 00:22:10 +0530

    RabbitMQ AWS

--- SignInUser/ConfigurationService.cs
+++ SignInUser/ConfigurationService.cs
@@ -9,10 +9,10 @@ public static class ConfigurationService
 
             x.UsingRabbitMq((ctx, cfg) => {
 
-                cfg.Host("localhost", "/", c =>
+                cfg.Host("b-e6da7d3b-c83d-4436-af6f-09e83fe7e80d.mq.eu-north-1.amazonaws.com", 5671, "/", c =>
                 {
-                    c.Username("guest");
-                    c.Password("guest");
+                    c.Username("copartner");
+                    c.Password("Cop@rtn$r%123");
                 });
 
                 cfg.ConfigureEndpoints(ctx);
