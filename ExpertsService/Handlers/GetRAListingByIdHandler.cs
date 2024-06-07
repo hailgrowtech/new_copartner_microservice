c5552cc3b86b8073bceebd52b22eec016edf6c8f
DEEPAK NEGI <deepak.negi@alecom.se>
2024-06-07 08:49:26 +0530

    Changes Bug user visit

--- ExpertsService/Handlers/GetRAListingByIdHandler.cs
+++ ExpertsService/Handlers/GetRAListingByIdHandler.cs
@@ -1,9 +1,11 @@
-﻿using ExpertService.Queries;
+﻿using ExpertService.Models;
+using ExpertService.Queries;
 using ExpertsService.Dtos;
 using MassTransit;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using MigrationDB.Data;
+using Ocelot.RequestId;
 
 namespace ExpertsService.Handlers;
 public class GetRAListingByIdHandler : IRequestHandler<GetRAListingByIdQuery, IEnumerable<RAListingDataDto>>
@@ -15,18 +17,18 @@ public class GetRAListingByIdHandler : IRequestHandler<GetRAListingByIdQuery, IE
     {
         // Calculate the number of records to skip
         int skip = (request.page - 1) * request.pageSize;
-        var query = from usr in _dbContext.Users
+        var query1 = from usr in _dbContext.Users
                     join sub in _dbContext.Subscribers on usr.Id equals sub.UserId into subscriberJoin
                     from sub in subscriberJoin.DefaultIfEmpty()
                     join wallet in _dbContext.Wallets on sub.Id equals wallet.SubscriberId into walletJoin
                     from wallet in walletJoin.DefaultIfEmpty()
-                    join aff in _dbContext.AffiliatePartners on usr.AffiliatePartnerId equals aff.Id into affiliatePartnerJoin
+                    join aff in _dbContext.AffiliatePartners on wallet.AffiliatePartnerId equals aff.Id into affiliatePartnerJoin
                     from aff in affiliatePartnerJoin.DefaultIfEmpty()
                     join exp in _dbContext.Experts on wallet.ExpertsId equals exp.Id into expertJoin
                     from exp in expertJoin.DefaultIfEmpty()
                     join subscr in _dbContext.Subscriptions on sub.SubscriptionId equals subscr.Id into subscriptionJoin
                     from subscr in subscriptionJoin.DefaultIfEmpty()
-                    where usr.ExpertsID == request.Id
+                    where exp.Id == request.Id && sub.isActive == true 
                     select new RAListingDataDto
                     {
                         RAName = exp.Name,
@@ -43,7 +45,35 @@ public class GetRAListingByIdHandler : IRequestHandler<GetRAListingByIdQuery, IE
                         GST = exp.GST
                     };
 
-        var result = await query.Skip(skip).Take(request.pageSize).ToListAsync(cancellationToken);
+        var query2 = from usr in _dbContext.Users
+                     join sub in _dbContext.Subscribers on usr.Id equals sub.UserId into subscriberJoin
+                     from sub in subscriberJoin.DefaultIfEmpty()
+                     join wallet in _dbContext.Wallets on sub.Id equals wallet.SubscriberId into walletJoin
+                     from wallet in walletJoin.DefaultIfEmpty()
+                     join aff in _dbContext.AffiliatePartners on wallet.AffiliatePartnerId equals aff.Id into affiliatePartnerJoin
+                     from aff in affiliatePartnerJoin.DefaultIfEmpty()
+                     join exp in _dbContext.Experts on wallet.ExpertsId equals exp.Id into expertJoin
+                     from exp in expertJoin.DefaultIfEmpty()
+                     join subscr in _dbContext.Subscriptions on sub.SubscriptionId equals subscr.Id into subscriptionJoin
+                     from subscr in subscriptionJoin.DefaultIfEmpty()
+                     where usr.ExpertsID == request.Id && usr.IsDeleted == false
+                     select new RAListingDataDto
+                     {
+                         RAName = exp.Name,
+                         UserJoiningDate = usr.CreatedOn,
+                         SubscribeDate = sub.CreatedOn,
+                         UserMobileNo = usr.Id != null ? usr.MobileNumber : null,
+                         APName = (aff.Id != null && aff.Id.ToString().Length > 10) ? aff.Name : ((usr.Id != null && usr.ExpertsID != null && usr.ExpertsID.ToString().Length > 5) ? "SELF" : "ORGANIC"),
+                         Amount = wallet.RAAmount,
+                         Subscription = subscr.ServiceType ?? "No Subscrption",
+                         PlanType = subscr.PlanType ?? "No Plan",
+                         TransactionId = sub.TransactionId,
+                         SubscriptionAmount = subscr.Amount,
+                         LegalName = exp.LegalName,
+                         GST = exp.GST
+                     };
+
+        var result = await query1.Union(query2).Skip(skip).Take(request.pageSize).ToListAsync(cancellationToken);
         return result;
     }
 }
--- SubscriptionService/Handlers/CreateSubscriberHandler.cs
+++ SubscriptionService/Handlers/CreateSubscriberHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using MigrationDB.Data;
 using MigrationDB.Model;
 using SubscriptionService.Commands;
@@ -18,12 +19,25 @@ public class CreateSubscriberHandler : IRequestHandler<CreateSubscriberCommand, 
 
     public async Task<Subscriber> Handle(CreateSubscriberCommand request, CancellationToken cancellationToken)
     {
-        var entity = request.Subscriber;
-        await _dbContext.Subscribers.AddAsync(entity);
-        await _dbContext.SaveChangesAsync(cancellationToken);
-        // Process subscriber in Wallet and save transaction
-       // _subscriberBusineessProcessor.ProcessSubscriberWallet(entity.Id); // Later  need to do via RabbitMQ
-        request.Subscriber.Id = entity.Id;
-        return request.Subscriber;
+        var validateData = await _dbContext.Subscribers
+            .FirstOrDefaultAsync(a => a.TransactionId == request.Subscriber.TransactionId, cancellationToken);
+
+        if (validateData == null)
+        {
+            var entity = request.Subscriber;
+            await _dbContext.Subscribers.AddAsync(entity, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            // Process subscriber in Wallet and save transaction
+            // _subscriberBusinessProcessor.ProcessSubscriberWallet(entity.Id); // Later need to do via RabbitMQ
+
+            request.Subscriber.Id = entity.Id;
+            return request.Subscriber;
+        }
+        else
+        {
+            return null;
+        }
     }
+
 }
