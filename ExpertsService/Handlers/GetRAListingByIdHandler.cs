e69f5b9c3cd5bbd33997eb63c2b1ee6efda5a7c1
DEEPAK NEGI <deepak.negi@alecom.se>
2024-06-20 14:07:45 +0530

    UTC IST Date Issue

--- ExpertsService/Handlers/GetRAListingByIdHandler.cs
+++ ExpertsService/Handlers/GetRAListingByIdHandler.cs
@@ -20,25 +20,25 @@ public class GetRAListingByIdHandler : IRequestHandler<GetRAListingByIdQuery, IE
         // Calculate the number of records to skip
         int skip = (request.page - 1) * request.pageSize;
         var query1 = from usr in _dbContext.Users
-                    join sub in _dbContext.Subscribers on usr.Id equals sub.UserId into subscriberJoin
-                    from sub in subscriberJoin.DefaultIfEmpty()
-                    join wallet in _dbContext.Wallets on sub.Id equals wallet.SubscriberId into walletJoin
-                    from wallet in walletJoin.DefaultIfEmpty()
-                    join aff in _dbContext.AffiliatePartners on usr.AffiliatePartnerId equals aff.Id into affiliatePartnerJoin
-                    from aff in affiliatePartnerJoin.DefaultIfEmpty()
-                    join exp in _dbContext.Experts on wallet.ExpertsId equals exp.Id into expertJoin
-                    from exp in expertJoin.DefaultIfEmpty()
-                    join subscr in _dbContext.Subscriptions on sub.SubscriptionId equals subscr.Id into subscriptionJoin
-                    from subscr in subscriptionJoin.DefaultIfEmpty()
-                    where exp.Id == request.Id && (sub.isActive == true || sub.IsDeleted == false)
+                     join sub in _dbContext.Subscribers on usr.Id equals sub.UserId into subscriberJoin
+                     from sub in subscriberJoin.DefaultIfEmpty()
+                     join wallet in _dbContext.Wallets on sub.Id equals wallet.SubscriberId into walletJoin
+                     from wallet in walletJoin.DefaultIfEmpty()
+                     join aff in _dbContext.AffiliatePartners on usr.AffiliatePartnerId equals aff.Id into affiliatePartnerJoin
+                     from aff in affiliatePartnerJoin.DefaultIfEmpty()
+                     join exp in _dbContext.Experts on wallet.ExpertsId equals exp.Id into expertJoin
+                     from exp in expertJoin.DefaultIfEmpty()
+                     join subscr in _dbContext.Subscriptions on sub.SubscriptionId equals subscr.Id into subscriptionJoin
+                     from subscr in subscriptionJoin.DefaultIfEmpty()
+                     where exp.Id == request.Id && (sub.isActive == true || sub.IsDeleted == false)
                      select new RAListingDataDto
-                    {
-                        RAName = exp.Name,
-                        UserJoiningDate = usr.CreatedOn.ToIST(),
-                        SubscribeDate = sub.CreatedOn.ToIST(),
-                        UserMobileNo = usr.Id != null ? usr.MobileNumber : null,
-                        User = usr,
-                        APName = (aff.Id != null && aff.Id.ToString().Length > 10)
+                     {
+                         RAName = exp.Name,
+                         UserJoiningDate = usr.CreatedOn,
+                         SubscribeDate = sub.CreatedOn,
+                         UserMobileNo = usr.Id != null ? usr.MobileNumber : null,
+                         User = usr,
+                         APName = (aff.Id != null && aff.Id.ToString().Length > 10)
                              ? aff.Name
                              : ((usr.Id != null && usr.ExpertsID != null && usr.ExpertsID == subscr.ExpertsId)
                                  ? "SELF"
@@ -46,15 +46,15 @@ public class GetRAListingByIdHandler : IRequestHandler<GetRAListingByIdQuery, IE
                                      ? (_dbContext.Experts.FirstOrDefault(e => e.Id == usr.ExpertsID).Name)
                                      : "ORGANIC")),
                          Amount = wallet.RAAmount,
-                        Subscription = subscr.ServiceType ?? "No Subscrption",
-                        PlanType = subscr.PlanType ?? "No Plan",
-                        TransactionId = sub.TransactionId,
-                        SubscriptionAmount = subscr.Amount,
-                        LegalName = exp.LegalName,
-                        GST = exp.GST,
-                        InvoiceId = sub.InvoiceId,
-                        PremiumTelegramChannel = sub.PremiumTelegramChannel,
-                         GSTAmount = sub.GSTAmount , // Handle null value
+                         Subscription = subscr.ServiceType ?? "No Subscrption",
+                         PlanType = subscr.PlanType ?? "No Plan",
+                         TransactionId = sub.TransactionId,
+                         SubscriptionAmount = subscr.Amount,
+                         LegalName = exp.LegalName,
+                         GST = exp.GST,
+                         InvoiceId = sub.InvoiceId,
+                         PremiumTelegramChannel = sub.PremiumTelegramChannel,
+                         GSTAmount = sub.GSTAmount, // Handle null value
                          TotalAmount = sub.TotalAmount // Handle null value
                      };
 
@@ -75,7 +75,7 @@ public class GetRAListingByIdHandler : IRequestHandler<GetRAListingByIdQuery, IE
                         //&& exp.Name != null
 
                         && !usr.IsDeleted
-                       // && (sub.isActive || !sub.IsDeleted)
+                     // && (sub.isActive || !sub.IsDeleted)
                      select new RAListingDataDto
                      {
                          RAName = exp.Name,
@@ -98,7 +98,7 @@ public class GetRAListingByIdHandler : IRequestHandler<GetRAListingByIdQuery, IE
                      };
 
         var result = await query1.Union(query2).Skip(skip).Take(request.pageSize).ToListAsync(cancellationToken);
-        return result;
+        return result.Select(e => e.ConvertAllDateTimesToIST()).ToList(); ;
     }
 }
 
--- SubscriptionService/Handlers/GetSubscriptionByIdHandler.cs
+++ SubscriptionService/Handlers/GetSubscriptionByIdHandler.cs
@@ -1,24 +1,33 @@
-﻿using MediatR;
+﻿using CommonLibrary.Extensions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using MigrationDB.Data;
 using MigrationDB.Model;
 using SubscriptionService.Queries;
 
-namespace SubscriptionService.Handlers
-{
-    public class GetSubscriptionByIdHandler : IRequestHandler<GetSubscriptionByIdQuery, Subscription>
-    {
-        private readonly CoPartnerDbContext _dbContext;
-        public GetSubscriptionByIdHandler(CoPartnerDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
+namespace SubscriptionService.Handlers;
 
-        public async Task<Subscription> Handle(GetSubscriptionByIdQuery request, CancellationToken cancellationToken)
-        {
-            var subscriptionMstsList = await _dbContext.Subscriptions.Include(s => s.Experts).Where(a => a.Id == request.Id && a.IsDeleted!= true).SingleOrDefaultAsync(cancellationToken: cancellationToken);
-            return subscriptionMstsList;
-        }
+public class GetSubscriptionByIdHandler : IRequestHandler<GetSubscriptionByIdQuery, Subscription>
+{
+    private readonly CoPartnerDbContext _dbContext;
+
+    public GetSubscriptionByIdHandler(CoPartnerDbContext dbContext)
+    {
+        _dbContext = dbContext;
     }
 
+    public async Task<Subscription> Handle(GetSubscriptionByIdQuery request, CancellationToken cancellationToken)
+    {
+        var subscription = await _dbContext.Subscriptions
+            .Include(s => s.Experts)
+            .Where(a => a.Id == request.Id && !a.IsDeleted)
+            .SingleOrDefaultAsync(cancellationToken);
+
+        if (subscription != null)
+        {
+            subscription.ConvertAllDateTimesToIST(); // Assuming this method modifies the subscription object
+        }
+
+        return subscription;
+    }
 }
