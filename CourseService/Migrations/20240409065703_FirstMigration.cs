97cef3347ee9fd43050b9caef499f6cf3fe65a17
Akash Khairal <akashkhairal@gmail.com>
2024-06-25 16:36:21 +0530

    ocelot fix

--- CourseService/Migrations/20240409065703_FirstMigration.cs
+++ CourseService/Migrations/20240409065703_FirstMigration.cs
@@ -11,19 +11,19 @@ namespace CourseService.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "ExpertsType",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    ExpertType = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_ExpertsType", x => x.Id);
-                });
+            //migrationBuilder.CreateTable(
+            //    name: "ExpertsType",
+            //    columns: table => new
+            //    {
+            //        Id = table.Column<int>(type: "int", nullable: false)
+            //            .Annotation("SqlServer:Identity", "1, 1"),
+            //        ExpertType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+            //        Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_ExpertsType", x => x.Id);
+            //    });
 
             migrationBuilder.CreateTable(
                 name: "User",
