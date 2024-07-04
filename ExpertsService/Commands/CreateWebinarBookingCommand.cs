2bb148d53f79e08c276a0c1f24c0af6523d1b53d
Akash Khairal <akashkhairal@gmail.com>
2024-07-04 17:21:17 +0530

    WebinarBooking apis

---
+++ ExpertsService/Commands/CreateWebinarBookingCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using MigrationDB.Model;
+
+namespace ExpertsService.Commands;
+
+public record CreateWebinarBookingCommand(WebinarBooking WebinarBooking) : IRequest<WebinarBooking>;
